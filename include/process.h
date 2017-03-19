bfcccc15bd907b198c8354364eaf59b47f3620a9
LN <209758171@163.com>
2017-03-19 23:17:07 +0800

    add process.h

---
+++ include/process.h
@@ -0,0 +1,28 @@
+#ifndef PROCESSPOOL_H
+#define PROCESSPOOL_H
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/epoll.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+class process
+{
+public:
+    process() : m_pid( -1 ){}
+
+public:
+    pid_t m_pid;
+    int m_pipefd[2];
+};
