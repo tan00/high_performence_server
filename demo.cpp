23953fd521999c855e3965a6fe1737d893f9b29c
LN <209758171@163.com>
2017-03-23 17:00:48 +0800

    nothing

--- demo.cpp
+++ demo.cpp
@@ -4,10 +4,22 @@
 
 int main()
 {
+    int ret = 0;
     sem sem1;
     locker locker1;
     cond cond1;
     err_msg("sizof sig = [%d]" ,sizeof(SIGTERM));
+
+    char *logVal = NULL;
+    logVal = getenv("UNIONLOG");
+    printf("varPtr=[%X] \n",logVal);
+    printf("var=[%s] \n",logVal);
+    ret = access(logVal,R_OK|W_OK|X_OK);
+    if(ret<0)
+    {
+        printf("access err ret=[%d] errno=[%d]  strerror=[%s]\n",ret,errno ,strerror(errno));
+        return -1;
+    }
 
     return 0;
 }
